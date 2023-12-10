616631f5c306946518fe3f450507e67f85128620
real2two <26689496+real2two@users.noreply.github.com>
2023-12-10 18:33:52 -0500

    Made variables work (lazy code)

--- src/func/displayPage.ts
+++ src/func/displayPage.ts
@@ -1,8 +1,11 @@
 import { ActionRow, Button } from "interactions.js";
+import { Interpreter, RandomParser, RangeParser, FiftyFiftyParser, IfStatementParser, SliceParser, StrictVarsParser, IntegerTransformer, StringTransformer } from 'tagscript';
 import { getPage, setUserMessage } from "../lib/db";
 import { evaluteExpression } from "./evaluteExpression";
 import { transformVariables } from "./transformVariables";
 import type { TransformedVariables } from "../types/Page";
+
+const ts = new Interpreter(new SliceParser(), new FiftyFiftyParser(), new RangeParser(), new RandomParser(), new IfStatementParser(), new StrictVarsParser());
 
 export async function displayPage(interaction: any, user: { bookId: number; pageId: number; messageId: string; }, fetchedVariables?: TransformedVariables) {
   const { bookId, pageId, messageId } = user;
@@ -19,6 +22,15 @@ export async function displayPage(interaction: any, user: { bookId: number; page
   }
 
   const variables = fetchedVariables || await transformVariables(interaction.user.id);
+  const transformedVariables: { [key: string]: IntegerTransformer | StringTransformer} = {};
+  for (const [key, value] of Object.entries(variables)) {
+    transformedVariables[key] = new IntegerTransformer(`${value}`);
+  }
+
+  transformedVariables["userId"] = new StringTransformer(interaction.user.id);
+
+  const parseText = async (text: string) => (await ts.run(text, transformedVariables)).body || "An unexpected error has occurred while parsing text";
+
   const components = [];
   for (const choice of pageInfo.choices) {
     if (evaluteExpression(choice.isVisibleCondition, variables)) {
@@ -31,7 +43,7 @@ export async function displayPage(interaction: any, user: { bookId: number; page
 
       if (choice.style) button.setStyle(choice.style);
       if (choice.emoji) button.setEmoji(choice.emoji);
-      if (choice.label) button.setLabel(choice.label);
+      if (choice.label) button.setLabel(await parseText(choice.label));
 
       button.setDisabled(!choice.gotos.find(({ condition }) => evaluteExpression(condition, variables)));
       button.setCustomId(`choice-${interaction.user.id}-${pageInfo.choices.indexOf(choice)}`);
@@ -39,9 +51,37 @@ export async function displayPage(interaction: any, user: { bookId: number; page
       actionRow.addComponent(button);
     }
   }
+
+  const embeds = pageInfo.embeds;
+  for (const embed of pageInfo.embeds) {
+    if (embed.title) embed.title = await parseText(embed.title);
+    if (embed.description) embed.description = await parseText(embed.description);
+    if (embed.author) {
+      if (embed.author?.name) embed.author.name = await parseText(embed.author.name);
+      if (embed.author?.url) embed.author.url = await parseText(embed.author.url);
+      if (embed.author?.icon_url) embed.author.icon_url = await parseText(embed.author.icon_url);
+    }
+    if (embed.url) embed.url = await parseText(embed.url);
+    if (embed.fields) {
+      for (const field of embed.fields) {
+        if (field?.name) field.name = await parseText(field.name);
+        if (field?.value) field.value = await parseText(field.value);
+      }
+    }
+    if (embed.image) {
+      if (embed.image?.url) embed.image.url = await parseText(embed.image.url);
+    }
+    if (embed.thumbnail) {
+      if (embed.thumbnail?.url) embed.thumbnail.url = await parseText(embed.thumbnail.url);
+    }
+    if (embed.footer) {
+      if (embed.footer?.text) embed.footer.text = await parseText(embed.footer.text);
+      if (embed.footer?.icon_url) embed.footer.icon_url = await parseText(embed.footer.icon_url);
+    }
+  }
   
   const message = await interaction.editReply({
-    embeds: pageInfo.embeds,
+    embeds,
     components,
   });
 
